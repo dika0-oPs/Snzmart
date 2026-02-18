1afcf3324d24cfa54a1e38e2d4cdf5db72cbf7e8
dika0-oPs <dikasipalingcihuy@gmail.com>
2026-02-18 22:46:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    SB_URL: 'https://rymvedtafnzznedtahez.supabase.co',
+    SB_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5bXZlZHRhZm56em5lZHRhaGV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MTg4NTYsImV4cCI6MjA4Njk5NDg1Nn0.ne8w_ZpEijzndPiHpMxbrdVHUWqO1-xs-7twYlE0hrM',
+    BUCKET_NAME: 'produk-images',
+    OWNER_NAME: 'Snzmart',
+    ADMIN_AUTH: {
+        email: 'admin@g.a',
+        pw: 'admin'
+    }
+};
